8d0a85f0fe33d148a64d3f85789ef0a72ae16791
Soumi <Souminath98@gmail.com>
2024-11-13 12:33:51 +0530

    keyvault added change 2

--- main.tf
+++ main.tf
@@ -13,3 +13,48 @@ terraform {
         key                  = "ccpsterraform.tfstate"
     }
 }
+
+# RG Creation
+resource "azurerm_resource_group" "RG-githubaction-azure"{
+    name     = "rg-githubaction-cloudquickpocs"
+    location = "northeurope"
+}
+
+# Retrieve data - tenantid, spnid, subscriptionid etc
+data "azurerm_client_config" "current" {}
+
+# Create Keyvault
+resource "azurerm_key_vault" "quickpocskeyvault" {
+    name                       = "quickpocskeyvault001"
+    location                   = azurerm_resource_group.RG-githubaction-azure.location
+    resource_group_name        = azurerm_resource_group.RG-githubaction-azure.name
+    tenant_id                  = data.azurerm_client_config.current.tenant_id
+    sku_name                   = "premium" 
+    soft_delete_enabled        = true
+    soft_delete_retention_days = 7
+
+    access_policy {
+        tenant_id = data.azurerm_client_confirg.current.tenant_id
+        object_id = data.azurerm_client_confirg.current.object_id
+
+        key_permissions = [
+            "create",
+            "get",
+        ]
+
+        secret_permissions = [
+            "set",
+            "get",
+            "delete",
+            "purge",
+            "recover",
+        ]
+    }
+}
+
+# Create keyvault secret
+resource "azurerm_key_vault_secret" "quickkeyvaultsecrete" {
+    name = "testsecrete"
+    value = "00000"
+    key_vault_id = azurerm_key_vault.quickpocskeyvault.id
+}
